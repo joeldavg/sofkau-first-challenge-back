095b6ba439105c1de1bcec95debe3bcb7bf053ba
Joel Guzman <joel-guzman@hotmail.com>
2022-05-04 09:11:05 -0500

    first commit

---
+++ src/main/java/com/example/firstchallengeback/FirstChallengeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstchallengeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstChallengeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstChallengeBackApplication.class, args);
+    }
+
+}
